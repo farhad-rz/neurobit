d8f67454ae2a91bf2e9825213de06688aff7f195
farhad-rz <farhadqz10@outlook.com>
2025-06-27 04:45:22 +0330

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,75 @@
+// handle theming
+const root = document.documentElement;
+const themeToggleBtns = document.querySelectorAll('.theme-toggle');
+const savedTheme = localStorage.getItem('theme');
+
+// ----- 1. set the initial theme --------------------------------
+if (savedTheme) {
+    root.dataset.theme = savedTheme;      // restores last choice
+    themeToggleBtns.forEach(themeToggleBtn => {
+        themeToggleBtn.textContent = savedTheme === 'dark' ? '☀️' : '🌙';
+    });
+    
+} else {
+    root.dataset.theme = 'light';         // default
+}
+
+// ----- 2. click handler ----------------------------------------
+themeToggleBtns.forEach(themeToggleBtn => {
+    themeToggleBtn.addEventListener('click', () => {
+        const next = root.dataset.theme === 'dark' ? 'light' : 'dark';
+        root.dataset.theme = next;
+        localStorage.setItem('theme', next);
+        themeToggleBtn.textContent = next === 'dark' ? '☀️' : '🌙';
+    });
+});
+
+
+// ---------- MENU & GRID SPOT ----------
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+        const tgt = document.querySelector(link.getAttribute("href"));
+        if (tgt) tgt.scrollIntoView({ behavior: "smooth" });
+        document.getElementById("mobileMenu").classList.remove("open");
+    });
+});
+document.getElementById("openMenu").addEventListener("click", () => document.getElementById("mobileMenu").classList.add("open"));
+document.getElementById("closeMenu").addEventListener("click", () => {
+    document.getElementById("mobileMenu").classList.remove("open");
+});
+
+const R = document.documentElement;
+function setSpot(e) {
+    R.style.setProperty("--cursor-x", e.clientX + "px");
+    R.style.setProperty("--cursor-y", e.clientY + "px");
+}
+window.addEventListener("mousemove", setSpot);
+window.addEventListener("touchmove", e => setSpot(e.touches[0]));
+
+
+
+
+
+// ---------- SCRAMBLE CYCLE ----------
+const handleScramble = (text,  el_selector) => {
+    const el = document.querySelector(el_selector);
+    el.innerHTML = text;
+    new ScrambleText(el).play().start();
+};
+
+function cycle(el_selector) {
+    try {
+        handleScramble(TEXTS[i % TEXTS.length], el_selector);
+        i++; setTimeout(cycle, 5000);
+    } catch (error) {
+        // passs
+    }
+    
+    
+}
+
+// load animation libs
+const { animate, scroll, inView } = Motion;
+
+
